29ad9cea2c421eedd0a0f75d45b08f8d4c697eab
JoseMercedes16 <75501634+JoseMercedes16@users.noreply.github.com>
2022-02-21 18:11:20 -0400

    Put path to the buttons of the card component

--- src/Componets/Card.js
+++ src/Componets/Card.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import '../CSS/Cards.css'
 
-function Card({title, imageSRC,text}) {
+function Card({title, imageSRC,text, direccion}) {
     return (
         <div className='card text-center bg-dark'>
             <img className='card-img-to' src={imageSRC} alt='Card image cap'/>
             <div className='card-body text-light'>
                 <h4 className='card-title'>{title} </h4>
                 <p className='card-text text-secondary'>{text} </p>
-                    <a href='#!' className='btn btn-outline-secondary' > conose mas </a>
+                    <button onClick={direccion} className='btn btn-outline-secondary'>
+                    <a href={direccion}  > conose mas </a>
+                    </button>
             </div>
         </div>
     )
